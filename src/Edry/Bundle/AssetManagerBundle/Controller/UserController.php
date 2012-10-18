06476dfe19f6c396acc54c3eea21adec9ddcbb72
Ian B <ian@belcherent.com>
2012-10-18 19:17:01 +1100

    Add a security so all users must be logged in

---
+++ src/Edry/Bundle/AssetManagerBundle/Controller/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Edry\Bundle\AssetManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+
+/**
+ * @Route("/user")
+ */
+class UserController extends Controller
+{
+    /**
+     * @Route("/login", name="_login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $this->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="_user_check")
+     */
+    public function userCheckAction()
+    {
+        // The security layer will intercept this request
+    }
+
+    /**
+     * @Route("/logout", name="_logout")
+     */
+    public function logoutAction()
+    {
+        // The security layer will intercept this request
+    }
+
+}
